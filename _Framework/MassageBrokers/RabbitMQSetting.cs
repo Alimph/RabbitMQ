d4d8e3f7798378273d11bb539a4b1e035f91aeb6
ali.momeni11.am <ali.momeni11.am@gmail.com>
2025-02-03 10:39:14 +0330

    Add Port prop to RabbitMQSetting

--- _Framework/MassageBrokers/RabbitMQSetting.cs
+++ _Framework/MassageBrokers/RabbitMQSetting.cs
@@ -3,6 +3,7 @@
     public class RabbitMQSetting
     {
         public string HostName { get; set; } = null!;
+        public int Port { get; set; }
         public string UserName { get; set; } = null!;
         public string Password { get; set; } = null!;
     }
